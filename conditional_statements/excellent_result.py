b7ffdb320bbeddf7d000cf9296829fa9dd43f243
niki <niki.dimov86@gmail.com>
2021-03-14 12:10:32 +0200

    lab

---
+++ conditional_statements/excellent_result.py
@@ -0,0 +1,7 @@
+grade = float(input())
+
+if 5.5 <= grade <= 6:
+    print("Excellent!")
+
+
+
---
+++ conditional_statements/greater_number.py
@@ -0,0 +1,5 @@
+def greater_number(a, b):
+    return max(a, b)
+
+
+print(greater_number(int(input()), int(input())))
